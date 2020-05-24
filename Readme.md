8ccdac9a0efcf3b5bfe9dbf0ef942e16fddf04a8
mertd <mertd@users.noreply.github.com>
2020-05-24 14:35:47 +0900

    initial commit

---
+++ Readme.md
@@ -0,0 +1,19 @@
+# Shovel Data
+
+A script that checks out all supported [scoop](https://scoop.sh/) buckets and collects the manifests in one searchable json file.
+
+## Motivation
+
+This can be prework for a SPA that allows to search for scoop apps without having to run a server. Also, I wanted to try out Go.
+
+## Run
+
+`go run shovel.go`
+
+## Use
+
+`https://mertd.github.io/shovel-data/manifests.json`
+
+## Licence
+
+MIT
---
+++ shovel.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"bytes"
+	"io"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	// clean up old runs
+	clean()
+	// clone buckets
+	buckets := []string{
+		"https://github.com/ScoopInstaller/Main.git",
+		"https://github.com/lukesampson/scoop-extras.git",
+	}
+	for i := 0; i < len(buckets); i++ {
+		clone(buckets[i])
+	}
+	// get a list of all json files
+	files, err := filepath.Glob("./*/bucket/*.json")
+	catch(err, "", "")
+	// read files
+	filesArray := readFilesToArray(files)
+	filesString := strings.Join(filesArray, ",")
+	filesString = "[" + filesString + "]"
+	// write to file
+	log.Println("Writing manifests to file")
+	write("manifests.json", filesString)
+	log.Println("Done")
+}
+
+func write(filename string, content string) {
+	file, err := os.Create("docs/" + filename)
+	catch(err, "", "")
+	defer file.Close()
+	_, err = io.WriteString(file, content)
+	catch(err, "", "")
+}
+
+func clone(url string) {
+	log.Println("Cloning bucket repository " + url)
+	var stdout bytes.Buffer
+	var stderr bytes.Buffer
+	cmd := exec.Command("git", "clone", url)
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stderr
+	err := cmd.Run()
+	catch(err, stdout.String(), stderr.String())
+}
+
+func readFilesToArray(files []string) []string {
+	log.Println("Reading manifests")
+	var result []string
+	for i := 0; i < len(files); i++ {
+		dat, err := ioutil.ReadFile(files[i])
+		catch(err, "", "")
+		result = append(result, string(dat))
+	}
+	return result
+}
+
+func clean() {
+	log.Println("Cleaning up previous runs (if any)")
+	files, err := filepath.Glob("./*/*")
+	catch(err, "", "")
+	for i := 0; i < len(files); i++ {
+		// don't delete our .git
+		if !strings.HasPrefix(files[i], ".git") && !strings.HasPrefix(files[i], "docs") {
+			err := os.RemoveAll(files[i])
+			catch(err, "", "")
+		}
+	}
+	log.Println("Cleaned up " + strconv.Itoa(len(files)) + " paths")
+}
+
+func catch(err error, stdout string, stderr string) {
+	if err != nil {
+		log.Println("ERROR")
+		log.Println(err)
+		if stdout != "" {
+			log.Println(stdout)
+		}
+		if stderr != "" {
+			log.Println(stderr)
+		}
+		log.Fatalln("Exiting shovel because of an error. Check the logging output above.")
+	}
+}

9806c0e85c2289044c604897ba89160e114e0dcb
Mert Demir <mertd@users.noreply.github.com>
2022-01-15 13:32:23 +0900

    convert

--- shovel.go
+++ shovel.go
@@ -115,10 +115,10 @@ func parseManifests(files []string) []string {
 }
 
 func extractManifestContents(manifest *gabs.Container) (string, string, string, string) {
-	homepage := manifest.Path("homepage");
-	version := manifest.Path("version");
-	description := manifest.Path("description");
-	github := manifest.Path("checkver.github");
+	homepage := manifest.Path("homepage").Data().(string);
+	version := manifest.Path("version").Data().(string);
+	description := manifest.Path("description").Data().(string);
+	github := manifest.Path("checkver.github").Data().(string);
 	return homepage, version, description, github
 }
 
